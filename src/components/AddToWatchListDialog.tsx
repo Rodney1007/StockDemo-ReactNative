f3d6cb7d9ea1ca901f7de21732a4e68a42bde187
Marros <newruc107@gmail.com>
2025-03-18 15:36:47 +0800

    將StockItem中加入自選股的對話視窗抽成一個元件

--- src/components/AddToWatchListDialog.tsx
+++ src/components/AddToWatchListDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ConfirmDialog from './ConfirmDialog';
 import { StockData } from './StockData';
 import watchListService from '../services/WatchListService';
@@ -10,13 +10,9 @@ interface AddToWatchListDialogProps {
   onClose: () => void;
 }
 
-const AddToWatchListDialog: React.FC<AddToWatchListDialogProps> = ({
-  visible,
-  stock,
-  onSuccess,
-  onClose,
-}) => {
-  const handleConfirm = async () => {
+class AddToWatchListDialog extends Component<AddToWatchListDialogProps> {
+  handleConfirm = async () => {
+    const { stock, onSuccess, onClose } = this.props;
     const success = await watchListService.addToWatchList(stock);
     if (success && onSuccess) {
       onSuccess();
@@ -24,15 +20,19 @@ const AddToWatchListDialog: React.FC<AddToWatchListDialogProps> = ({
     onClose();
   };
 
-  return (
-    <ConfirmDialog
-      visible={visible}
-      title="加入自選"
-      message={`是否將 ${stock.name}(${stock.symbol}) 加入自選股？`}
-      onConfirm={handleConfirm}
-      onCancel={onClose}
-    />
-  );
-};
+  render() {
+    const { visible, stock, onClose } = this.props;
 
-export default AddToWatchListDialog; 
+    return (
+      <ConfirmDialog
+        visible={visible}
+        title="加入自選"
+        message={`是否將 ${stock.name}(${stock.symbol}) 加入自選股？`}
+        onConfirm={this.handleConfirm}
+        onCancel={onClose}
+      />
+    );
+  }
+}
+
+export default AddToWatchListDialog;
